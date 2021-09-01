97d9e5aac6aa5267469fc2a3277adabc5c9d56b1
zakzc <zak.zangrando@outlook.com>
2021-09-01 21:34:02 +0200

    Test for image background implementation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import configureStore from "./store/configureStore";
 // comp
 // import AppView from "./app/components/appView_baseComp";
 import Routes from "./app/components/appRoutes";
+import appBackground from "./app/components/assets/img/LPsCollection.jpg";
 
 const store = configureStore();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <div
+        style={{
+          backgroundImage: `url(${appBackground})`,
+          opacity: 0.8,
+          height: "100%",
+        }}
+      >
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 };
